e9bb59e569c666f8f0b9937563f8c2fb0982b413
DxvilDevs <107037653+DxvilDevs@users.noreply.github.com>
2026-01-17 19:47:33 +0000

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/exam-command-centre/" // <-- CHANGE to "/<your-repo-name>/"
+  base: "/examcommandfixedhopefully/" // <-- CHANGE to "/<your-repo-name>/"
 });
