8884f8594a30a673791ac7f981ac5a06b07c1013
DxvilDevs <107037653+DxvilDevs@users.noreply.github.com>
2026-01-18 19:39:53 +0000

    Create focus.js

---
+++ frontend/src/api/focus.js
@@ -0,0 +1,5 @@
+import { api } from "./client";
+export const focusApi = {
+  start: (topic, task_label) => api("/focus/start", { method: "POST", body: { topic, task_label } }),
+  end: (id, minutes, completed = true) => api("/focus/end", { method: "POST", body: { id, minutes, completed } })
+};
