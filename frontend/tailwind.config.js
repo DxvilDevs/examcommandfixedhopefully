2ff828ca967da5dba9fab227685d5da24ff68215
DxvilDevs <107037653+DxvilDevs@users.noreply.github.com>
2026-01-24 18:52:23 +0000

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        'dark-bg': '#0a0a1f',
+      }
+    }
+  },
   plugins: []
 };
